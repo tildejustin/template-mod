e6218ee11706248552716eb6ce54aeae9df056b3
tildejustin <spacepepper3.14@gmail.com>
2023-08-21 17:27:23 -0400

    init commit

---
+++ src/main/java/dev/tildejustin/example/mixin/MinecraftClientMixin.java
@@ -0,0 +1,8 @@
+package dev.tildejustin.example.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftClient.class)
+public abstract class MinecraftClientMixin {
+}
